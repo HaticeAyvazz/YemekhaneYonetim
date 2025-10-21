e41b34a42b7752b1164de6dbf93576324a8dc3a3
Hatice Ayvaz <haticeayvaz2941@gmail.com>
2025-10-21 11:25:01 +0300

    fix: project sturct

--- YemekhaneYonetim-main/YemekhaneYonetim-main/src/main/java/com/example/yemekhaneyonetimsistemi/Repository/IYemekRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.yemekhaneyonetimsistemi.Repository;
-
-import com.example.yemekhaneyonetimsistemi.entity.Yemek;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface IYemekRepository extends JpaRepository<Yemek, Integer> {
-}
